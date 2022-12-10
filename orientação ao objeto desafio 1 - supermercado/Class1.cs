a2dbebe788c70a8329a92128264c49b98c5e1d40
ViniciusAraujoMoraes <moraes.vinicius.ma@gmail.com>
2022-12-10 15:17:01 -0300

    Adicionar arquivos de projeto.

---
+++ orientação ao objeto desafio 1 - supermercado/Class1.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace orientação_ao_objeto_desafio_1___supermercado
+{
+    public class Produto
+    {
+        public string nome;
+        public string fabricante;
+        public float codigoDeBarras;
+        public double valorUnitario;
+        public double valorDeCompra;
+        public int quantidade;
+    }
+}
---
+++ orientação ao objeto desafio 1 - supermercado/Program.cs
@@ -0,0 +1,33 @@
+﻿using orientação_ao_objeto_desafio_1___supermercado;
+
+Produto Produto1 = new Produto();
+
+Produto1.nome = "Banana Nanica";
+Produto1.fabricante = "MD Alimentos";
+Produto1.codigoDeBarras = 001;
+Produto1.valorUnitario = 2.25;
+Produto1.valorDeCompra = 1.75;
+Produto1.quantidade = 80;
+
+Produto Produto2 = new Produto();
+
+Produto2.nome = "Cenoura";
+Produto2.fabricante = "MD Alimentos";
+Produto2.codigoDeBarras = 002;
+Produto2.valorUnitario = 1.90;
+Produto2.valorDeCompra = 1.25;
+Produto2.quantidade = 70;
+
+Produto Produto3 = new Produto();
+
+Produto3.nome = "Batata";
+Produto3.fabricante = "MD Alimentos";
+Produto3.codigoDeBarras = 003;
+Produto3.valorUnitario = 2.50;
+Produto3.valorDeCompra = 1.85;
+Produto3.quantidade = 20;
+
+double totalDaCompra = (Produto3.valorDeCompra + Produto2.valorDeCompra + Produto1.valorDeCompra);
+
+
+Console.WriteLine("o valor da compra é " + totalDaCompra);
